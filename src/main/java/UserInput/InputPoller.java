cfff8103126fee7086810d3bcee568239c8c5b81
Pauyy <pauyyk@gmail.com>
2023-04-08 11:56:11 +0200

    Poll input for more than once per frame

--- src/main/java/UserInput/InputPoller.java
+++ src/main/java/UserInput/InputPoller.java
@@ -4,22 +4,42 @@ import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.util.Stack;
 
-public class InputPoller {
+public class InputPoller implements Runnable{
 
     private final Terminal terminal;
 
+    private Stack<KeyStroke> stack;
+    private Thread selfThread;
+
     public InputPoller(Terminal terminal){
         this.terminal = terminal;
+        stack = new Stack<>();
+        selfThread = new Thread(this);
+        selfThread.start();
     }
 
 
-    public KeyStroke poll(){
-        try {
-            return terminal.pollInput();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public KeyStroke get(){
+        if(stack.isEmpty())
             return null;
+        KeyStroke keyStroke = stack.pop();
+        stack.clear();
+        return keyStroke;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            KeyStroke polled;
+            try {
+                polled = terminal.pollInput();
+                if (polled != null)
+                    stack.add(polled);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
